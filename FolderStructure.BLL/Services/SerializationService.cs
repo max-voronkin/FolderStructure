cb7cd30c3f1e6f2a0eff47813434514bef98e3d0
Max Voronkin <voronkin.max.wm@gmail.com>
2023-04-27 21:05:30 +0300

    Created serialization service to store file structure

---
+++ FolderStructure.BLL/Services/SerializationService.cs
@@ -0,0 +1,46 @@
+﻿using FolderStructure.DAL;
+using FolderStructure.DAL.DTO;
+using FolderStructure.DAL.Entities;
+using System;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Xml.Serialization;
+
+namespace FolderStructure.BLL.Services
+{
+    public class SerializationService
+    {
+        private static readonly string fileName = "structure.xml";
+        string baseDirecory = AppDomain.CurrentDomain.BaseDirectory;
+
+        public void SerializeStructure()
+        {
+            if (!Directory.Exists($"{baseDirecory}\\Files"))
+            {
+                Directory.CreateDirectory($"{baseDirecory}\\Files");
+            }
+            SerializableFolder structure = GetSerializibleStructure();
+            XmlSerializer xmlFromat = new XmlSerializer(typeof(SerializableFolder));
+            using (Stream fStream = new FileStream($"{baseDirecory}\\Files\\{fileName}", FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                xmlFromat.Serialize(fStream, structure);
+            }
+        }
+
+        private SerializableFolder GetSerializibleStructure()
+        {
+            using (var context = new StructureDBContext())
+                return ConvertToSerializible(context.Folders.Where(f => f.ParrentId == null).Include(s => s.SubFolders).FirstOrDefault());
+        }
+        private SerializableFolder ConvertToSerializible(Folder folder)
+        {
+            var dto = new SerializableFolder()
+            {
+                Name = folder.Name,
+                SubFolders = folder.SubFolders?.Select(s => ConvertToSerializible(s)).ToList()
+            };
+            return dto;
+        }
+    }
+}
