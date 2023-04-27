11fb4ccd26fcb315f94b891df09165737589feea
Max Voronkin <voronkin.max.wm@gmail.com>
2023-04-27 15:46:23 +0300

    Created custom exception for incorrect routes

---
+++ FolderStructure.BLL/Exceptions/IncorrectPathException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace FolderStructure.BLL.Exceptions
+{
+    public class IncorrectPathException : Exception
+    {
+        public IncorrectPathException(string message) : base(message) { }
+        public IncorrectPathException(string message, string nodeId) : base($"{message}, Node id: {nodeId}") { }
+    }
+}
